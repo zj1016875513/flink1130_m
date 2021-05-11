1e5a3e43a51bb494ad4ba1f8b3d902374ebbde0d
lzc <lzc@atguigu.cn>
2021-05-11 16:45:56 +0800

    update

--- src/main/java/com/atguigu/chapter06/Flink01_Project_PV_2.java
+++ src/main/java/com/atguigu/chapter06/Flink01_Project_PV_2.java
@@ -43,6 +43,7 @@ public class Flink01_Project_PV_2 {
     
                         out.collect(sum);
                     }
+                    
                 }
             })
             .print();
