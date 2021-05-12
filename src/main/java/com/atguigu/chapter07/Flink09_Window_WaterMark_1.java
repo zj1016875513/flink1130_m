2e29a1ade549ba9d5930a0c12b7ce0255a7f0c84
lzc <lzc@atguigu.cn>
2021-05-12 17:00:01 +0800

    update

---
+++ src/main/java/com/atguigu/chapter07/Flink09_Window_WaterMark_1.java
@@ -0,0 +1,74 @@
+package com.atguigu.chapter07;
+
+import com.atguigu.bean.WaterSensor;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Author lizhenchao@atguigu.cn
+ * @Date 2021/5/12 16:39
+ */
+public class Flink09_Window_WaterMark_1 {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.setInteger("rest.port", 20000);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        env.setParallelism(1);
+        
+        env
+            .socketTextStream("hadoop162", 9999)
+            .map(line -> {
+                String[] data = line.split(",");
+                return new WaterSensor(
+                    data[0],
+                    Long.valueOf(data[1]),
+                    Integer.valueOf(data[2]));
+                
+            })
+            .assignTimestampsAndWatermarks(
+                WatermarkStrategy
+                    .<WaterSensor>forMonotonousTimestamps()
+                    .withTimestampAssigner(new SerializableTimestampAssigner<WaterSensor>() {
+                        @Override
+                        public long extractTimestamp(WaterSensor element, // 事件时间的值必须是毫秒
+                                                     long recordTimestamp) {
+                            return element.getTs() * 1000;
+                        }
+                    })
+            )
+            .keyBy(WaterSensor::getId)
+            .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+            .process(new ProcessWindowFunction<WaterSensor, String, String, TimeWindow>() {
+                @Override
+                public void process(String key,
+                                    Context ctx,
+                                    Iterable<WaterSensor> elements,
+                                    Collector<String> out) throws Exception {
+                    int count = 0;
+                    for (WaterSensor ws : elements) {
+                        count++;
+                    }
+                    TimeWindow w = ctx.window();
+                    out.collect(
+                        "当前Key=" + key
+                            + "窗口: [ " + w.getStart() / 1000 + "," + w.getEnd() / 1000 + " ), "
+                            + "元素个数: " + count
+                    );
+                }
+            })
+            .print();
+        
+        try {
+            env.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
